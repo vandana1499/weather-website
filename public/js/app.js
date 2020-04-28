7f0225479a9d811d0ec73d1173a75f3a570d2696
vandana gupta <vandanagupta1499@gmail.com>
2020-04-28 17:39:23 +0530

    changes to certain files

--- public/js/app.js
+++ public/js/app.js
@@ -28,7 +28,8 @@ weatherForm.addEventListener('submit',(event)=>{
         {
          return  messageOne.textContent=data.error
         }
-         const forecast=data.description+".It is currently "+data.temperature+" degree.It feels like " +data.feelslike+" degree.Humidity is "+data.humidity
+        
+         const forecast=data.description+".It is currently "+data.temperature+" degree.It feels like " +data.feelslike+" degree. Humidity is  " + data.humidity
 
          messageOne.textContent=data.location;
          messageTwo.textContent=forecast;
